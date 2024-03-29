3744101b87640119542edd33aaeeb058265c622c
grimy.bunyip <grimy.bunyip@gmail.com>
2024-03-29 17:50:19 -0400

    2.2 fourstar cones

---
+++ lightCones/harmony/ForTomorrowsJourney.py
@@ -0,0 +1,20 @@
+from baseClasses.BaseCharacter import BaseCharacter
+from baseClasses.BaseLightCone import BaseLightCone
+
+class ForTomorrowsJourney(BaseLightCone):
+    def __init__(self,
+                 uptime=1.0/3.0,
+                **config):
+        self.loadConeStats('For Tomorrow\'s Journey')
+        self.setSuperposition(config)
+        self.uptime = uptime
+
+    def equipTo(self, char:BaseCharacter):
+        self.addStats(char)
+        if char.path == self.path:
+            char.addStat('ATK.percent',description=self.name,amount=0.12 + 0.04 * self.superposition)
+            char.addStat('DMG',description=self.name,amount=0.15 + 0.03 * self.superposition,uptime=self.uptime)
+            
+if __name__ == '__main__':
+    from settings.BaseConfiguration import Configuration
+    ForTomorrowsJourney(**Configuration).print()
---
+++ lightCones/nihility/BoundlessChoreo.py
@@ -0,0 +1,23 @@
+from baseClasses.BaseCharacter import BaseCharacter
+from baseClasses.BaseLightCone import BaseLightCone
+
+class BoundlessChoreo(BaseLightCone):
+    def __init__(self,
+                uptime:float=1.0,
+                **config):
+        self.loadConeStats(name='Boundless Choreo')
+        self.setSuperposition(config)
+        self.uptime = uptime
+
+    def equipTo(self, char:BaseCharacter):
+        self.addStats(char)
+        if char.path == self.path:
+            char.addStat('CR',description=self.name,
+                                    amount=0.06 + 0.02 * self.superposition)
+            char.addStat('CD',description=self.name,
+                                    amount=0.18 + 0.06 * self.superposition,
+                                    uptime=self.uptime)
+
+if __name__ == '__main__':
+    from settings.BaseConfiguration import Configuration
+    BoundlessChoreo(**Configuration).print()
