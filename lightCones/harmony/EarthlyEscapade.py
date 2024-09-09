7ed0351b7fcf1fc0c832dd0e5fe4ca836700e04f
grimy.bunyip <grimy.bunyip@gmail.com>
2024-09-08 20:07:44 -0400

    earthly escapade tweak

--- lightCones/harmony/EarthlyEscapade.py
+++ lightCones/harmony/EarthlyEscapade.py
@@ -4,14 +4,29 @@ from baseClasses.BaseLightCone import BaseLightCone
 class EarthlyEscapade(BaseLightCone):
     def __init__(self,
                 superposition:int=None,
+                uptime:float=1.0,
                 **config):
         self.loadConeStats('Earthly Escapade')
         self.setSuperposition(superposition,config)
+        self.uptime = uptime
 
     def equipTo(self, char:BaseCharacter):
         self.addStats(char)
         if char.path == self.path:
             char.addStat('CD',description=self.name,amount=0.25 + 0.07 * self.superposition)
+            
+            def applyTeamBuff(team):
+                for targetChar in team:
+                    targetChar.addStat('CR',
+                                description=f'{self.shortname} from {char.name}',
+                                amount=0.09 + 0.01 * self.superposition,
+                                uptime=self.uptime,)
+                    targetChar.addStat('CD',
+                                description=f'{self.shortname} from {char.name}',
+                                amount=0.21 + 0.07 * self.superposition,
+                                uptime=self.uptime,)
+                    
+            char.teamBuffList.append(applyTeamBuff)
             
 if __name__ == '__main__':
     from settings.BaseConfiguration import Configuration
